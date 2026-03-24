9629cdfb3000660222e43443c6fab73d5ed691bb
Journey1 <srujanboda14@gmail.com>
2026-03-24 10:54:17 +0530

    mail error rectified

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -156,14 +156,14 @@ STORAGES = {
     },
 }
 
-# Email settings
+# Email settings (Using Gmail SMTP with App Password)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Media files
 MEDIA_URL = '/media/'
